e666075ceea7e9161d0c55330c65a1b2176929a1
carl@isi.edu <carl@isi.edu>
2017-10-17 16:22:31 -0700

    Clean up errors, refactored certificate calls

--- export_synapse.py
+++ export_synapse.py
@@ -1,21 +1,5 @@
-from deriva_common import HatracStore, ErmrestCatalog, get_credential
 import synapse_utils
 import os
-
-
-# Need to use Deriva authentication agent before executing this
-credential = get_credential("synapse.isrd.isi.edu")
-
-objectstore = HatracStore('https','synapse.isrd.isi.edu',credentials=credential)
-catalog = ErmrestCatalog('https','synapse.isrd.isi.edu', 1, credentials=credential)
-
-
-study_entities = synapse_utils.get_synapse_studies(catalog)
-study_uri = study_entities.uri
-
-print('Identified %d studies' % len(study_entities))
-
-study_list = []
 
 protocol_types = {
     'PrcDsy20160101A': 'aversion',
@@ -24,20 +8,31 @@ protocol_types = {
     'PrcDsy20170613B': 'control'
 }
 
-for i in study_entities:
-    if protocol_types[i['Protocol']] == 'aversion':
-        i['Type'] = 'learner' if i['Learner'] == True else 'nonlearner'
-    else:
-        i['Type'] = protocol_types[i['Protocol']]
-    study_list.append(i)
+destdir = '/Users/carl/Desktop'
 
+
+def export_files(study_entities, dest):
     # Dump out the synapses to a local directory
 
-destdir = '/Users/carl/Desktop'
-current_dir = os.getcwd()
+    study_list = []
+    for i in study_entities:
+        if protocol_types[i['Protocol']] == 'aversion':
+            i['Type'] = 'learner' if i['Learner'] is True else 'nonlearner'
+        else:
+            i['Type'] = protocol_types[i['Protocol']]
+        study_list.append(i)
 
-try:
-    os.chdir(destdir)
-    synapse_utils.export_synapse_studies(objectstore, study_list)
-finally:
-    os.chdir(current_dir)
+    current_dir = os.getcwd()
+
+    try:
+        os.chdir(dest)
+        synapse_utils.export_synapse_studies(study_list, dest)
+    finally:
+        os.chdir(current_dir)
+
+
+study_entities = synapse_utils.get_synapse_studies()
+print('Identified %d studies' % len(study_entities))
+
+export_files(study_entities, destdir)
+print('Dumped studies')
