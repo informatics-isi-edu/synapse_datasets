a08e3fd218d830707c195d3e63a9cdbfc18400d1
Carl Kesselman <carl@isi.edu>
2018-04-20 20:10:41 -0700

    Added dump of paired data.

--- export_synapse.py
+++ export_synapse.py
@@ -1,4 +1,7 @@
 import synapse_utils
+import synapse_plot_utils as sp
+from synapse_plot_config import plot_synapses, trace_color, position_layout, step_buttons, studytypes
+
 from deriva.core import ErmrestCatalog, get_credential
 import deriva.core.versioned_catalog as vc
 
@@ -16,21 +19,30 @@ bag_metadata = {
 }
 
 destdir = '/Users/carl/Desktop'
+studyid = 'TYR'
+
+dumpfile = 'pairs-dump.pkl'
+studyset = sp.restore_studies(dumpfile)
+studylist = studyset['Studies']
+studyid = studyset['StudyID']
+study_types = studytypes(studylist)
+
 
 # Get the entity list of studies.....
-study_entities = synapse_utils.get_synapse_studies()
-print('Identified %d studies' % len(study_entities))
+#study_entities = synapse_utils.get_synapse_studies(studyid)
+#print('Identified %d studies' % len(study_entities))
 
 # This is a hack until we get get right version support in PathBuilder
-ermrest_version = vc.current_catalog_version(
-    ErmrestCatalog('https', 'synapse.isrd.isi.edu', 1,
-                   get_credential('synapse.isrd.isi.edu')))
+#ermrest_version = vc.current_catalog_version(
+#    ErmrestCatalog('https', 'synapse.isrd.isi.edu', 1,
+#                   get_credential('synapse.isrd.isi.edu')))
 
 # Set some additional bag metadata
-bag_metadata['ERMRest-Query'] = vc.versioned_path(study_entities.uri, ermrest_version)
-bag_metadata['ERMRest-Snapshot'] = ermrest_version
+#bag_metadata['ERMRest-Query'] = vc.versioned_path(study_entities.uri, ermrest_version)
+#bag_metadata['ERMRest-Snapshot'] = ermrest_version
 
 # now dump out the bag and publish to hatrac
-archive = synapse_utils.synapses_to_bag(study_entities, destdir, protocol_types, bag_metadata)
+#archive = synapse_utils.synapses_to_bag(study_entities, destdir, protocol_types, bag_metadata)
+archive = synapse_utils.studyset_to_bag(studyset, destdir, protocol_types, bag_metadata)
 
 print('Dumped studies')
